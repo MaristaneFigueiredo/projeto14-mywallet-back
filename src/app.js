95a48770102a95351d2f6981f60154f2c5d8638b
Maristane Figueiredo <maristanetecnologia@gmail.com>
2022-11-21 13:44:20 -0300

    alteracoes para deploy

--- src/app.js
+++ src/app.js
@@ -15,4 +15,6 @@ app.use(recordsRouter);
 app.use(logoutRouter);
 
 //Definição da porta
-app.listen(5000, () => console.log("Server running in port 5000"));
+//app.listen(5000, () => console.log("Server running in port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port ${port}`));
